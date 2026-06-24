51ed9f11a248dcca6ef565bb1843dacb7e5668b5
Adam <131621020+adamgizis@users.noreply.github.com>
2026-06-24 15:22:40 -0400

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,16 @@
+class Car:
+    def __init__(self, pos, speed):
+        self.pos = pos
+        self.speed = speed
+        self.time = 0
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        stack = []
+        for p, s in pair:  # Reverse Sorted Order
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)

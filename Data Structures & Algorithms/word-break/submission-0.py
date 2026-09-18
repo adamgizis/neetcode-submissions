6341bccc5e9a3ffe47c765942c60993804b04b4c
Adam <131621020+adamgizis@users.noreply.github.com>
2026-09-17 20:12:18 -0400

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        queue = deque()
+
+        seen_word = set()
+
+        queue.append(0)
+
+        while queue:
+            index= queue.popleft()
+            if index == len(s) :
+                return True
+            for i in range(index, len(s)):
+                if s[index:i+1] in wordDict and (s[index:i+1], index, i) not in seen_word:
+                    queue.append(i+1)
+                    seen_word.add((s[index:i+1], index, i))
+
+
+
+        return False
